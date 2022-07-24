cc9c97afce577b350bf109e1065006d3bc6475a7
Sefa <mail@sefakalkan.com>
2022-07-25 01:07:20 +0300

    0.25 /Archive implement edildi veri sayısı arttırıldı

--- webScraper.py
+++ webScraper.py
@@ -1,17 +1,22 @@
 import time
 import requests
-
+import csv
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import json
 
 
+
+
+
 def mainWebScraper():
-    scrapeWeb()
+
+    temp2 = scrapeWebFromArchive()
 
 
-def scrapeWeb():
+"""def scrapeWeb():
     # Create a new instance of the Firefox driver.
     tempCounter = 0
     webHolder = []
@@ -26,7 +31,7 @@ def scrapeWeb():
     # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")
 
-    """while True:
+    while True:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -37,7 +42,7 @@ def scrapeWeb():
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
-        last_height = new_height"""
+        last_height = new_height
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
 
@@ -56,33 +61,51 @@ def scrapeWeb():
 
     print(len(links))
 
-    pullDataFromWeb(webHolder)
+    pullDataFromWeb(webHolder)"""
 
 
 def pullDataFromWeb(webHolder):
 
-    for pages in webHolder:
-        print(pages)
-        #request with incognito mode
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
-        page = requests.get(pages, headers=headers, cookies={"over18": "1", "incognito": "1"})
+    textIndexHolder = []
+    clapHolder = []
+    followerHolder = []
 
+    for pages in webHolder:
+        tempTextHolder = []
+
+
+
+        page = requests.get(pages)
 
         clap, follower = aditionalValues(pages)
-
-
 
         soup = BeautifulSoup(page.content, 'html.parser')
 
         text = soup.find_all('p', class_='pw-post-body-paragraph')
 
-
-
         for texts in text:
-            print(texts.get_text())
-            # Prints the text of the paragraph
-        print(clap, follower)
-        print("//////////////////////////////////////////////////////////////////////////////////////////////")
+            tempTextHolder.append(texts.get_text())
+
+
+
+        textIndexHolder.append(tempTextHolder)
+        clapHolder.append(clap)
+        followerHolder.append(follower)
+
+    header = ["Clap", "Follower", "Text"]
+
+    with open('dataHolder.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(zip(clapHolder, followerHolder, textIndexHolder))
+    f.close()
+
+    data = pd.read_csv('dataHolder.csv')
+    print(data.head())
+
+
+
+
 
 
 def aditionalValues(page):
@@ -97,3 +120,65 @@ def aditionalValues(page):
     followerNumber = int(follower[0:followerIndex])
 
     return claps, followerNumber
+
+def scrapeWebFromArchive():
+
+    tempCounter = 0
+    webHolder = []
+    webRedirectingForMonth = []
+    webRedirectingForDay = []
+    page = requests.get("https://medium.com/tag/t%C3%BCrk%C3%A7e/archive/2015")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+
+
+
+    links = soup.find_all('a', class_ = "")
+
+
+
+    for x in range(2009,2011):
+        print("Finished year: " + str(x))
+        tempCounter = 0
+        webRedirectingForMonth = []
+        webRedirectingForDay = []
+
+        page = requests.get("https://medium.com/tag/t%C3%BCrk%C3%A7e/archive/"+str(x))
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        links = soup.find_all('a', class_="")
+        for link in links:
+            if (link.get('data-action-source') != None):
+                webHolder.append(link.get('href'))
+            else:
+                webRedirectingForMonth.append(link.get('href'))
+
+        for link in webRedirectingForMonth:
+            page = requests.get(link)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            links = soup.find_all('a', class_="")
+            for link in links:
+                if (link.get('data-action-source') != None):
+                    webHolder.append(link.get('href'))
+                else:
+                    webRedirectingForDay.append(link.get('href'))
+
+        for link in webRedirectingForDay:
+            page = requests.get(link)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            links = soup.find_all('a', class_="")
+            for link in links:
+                if (link.get('data-action-source') != None):
+                    webHolder.append(link.get('href'))
+
+
+    pullDataFromWeb(webHolder)
+
+    print("we have :",len(webHolder),"pages in total")
+
+
+
+
+
+
+
