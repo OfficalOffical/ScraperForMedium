0ef799e32e8f6c6871ca1a0feb42bced8f682510
Sefa <mail@sefakalkan.com>
2022-09-13 21:52:15 +0300

    Update README.MD

---
+++ tempSsodasld.py
@@ -0,0 +1,16 @@
+
+#write me a function that perfects web scraper
+
+import libraries
+import urllib2
+from bs4 import BeautifulSoup
+import csv
+import pandas as pd
+import time
+import requests
+from selenium import webdriver
+
+
+
+
+
